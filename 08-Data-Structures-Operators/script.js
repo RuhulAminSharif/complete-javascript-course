37b9f617204be6a0e90fb17ccd6bf4d566659a9f
Sharif <sharif.cse.ras@gmail.com>
2025-05-19 17:40:30 +0600

    (+)Optional Chaining

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -22,6 +22,20 @@ const mexicanFoods = new Set([
   "garlic",
 ]);
 
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 // Data needed for main part of the section
 const restaurant = {
   name: "Classico Italiano",
@@ -30,22 +44,10 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function (starterIndex, mainIndex) {
+  // ES6 enhanced object properties
+  openingHours,
+  // ES6 enhanced object methods format
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -65,11 +67,42 @@ const restaurant = {
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
+
+///////////////////////////////////////
+// Optional Chaining
+
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// with optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+for (const day of days) {
+  const status = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, we open at ${status}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
+
+//Arrays
+// const users = [{ name: "Jonas", email: "hello@jonas.io" }];
+const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+
+/*
 
 ///////////////////////////////////////
 // The for-of Loop
@@ -83,7 +116,6 @@ for (const [idx, item] of menu.entries() ) {
   console.log(`At index ${idx}, the value is ${item}`);
   
 }
-/*
 ///////////////////////////////////////
 // Logical Assignment Operators
 const rest1 = {
