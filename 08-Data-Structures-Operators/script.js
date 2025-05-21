126ffc4e0ee9dc322c71ef970ec1a4c02326b9cc
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2025-05-21 08:02:09 +0600

    (+)Map Data Structure

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -55,6 +55,80 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Working with Maps
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+rest.set(2, "Lisbon, Portugal");
+console.log(rest.set(3, "Rome, Italy"));
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open :D")
+  .set(false, "We are closed :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+rest.get(time > rest.get("open") && time < rest.get("close"))
+  ? console.log("We are open :D")
+  : console.log("We are closed :(");
+
+console.log(rest.has("categories"));
+console.log(rest);
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+// rest.clear();
+console.log(rest);
+
+// rest.set([1, 2], "Test");
+// console.log(rest.get([1, 2])); // undefined
+
+const arr = [1, 2];
+rest.set(arr, "Test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest.get(arr));
+console.log(rest);
+
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct!"],
+  [false, "Try again!"],
+]);
+console.log(question);
+
+// convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+console.log(hoursMap.get("thu").open);
+
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt("Your answer"));
+// console.log(answer);
+// console.log(question.get(answer === question.get("correct")));
+
+// Convert map to array
+console.log([...question]);
+console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+/*
+
+///////////////////////////////////////
 // Sets
 const ordersSet = new Set([
   "Pasta",
@@ -139,7 +213,6 @@ console.log("Symmetric Difference:", uniqueItalianAndMexicanFoods);
 console.log(italianFoods.isDisjointFrom(mexicanFoods));
 
 
-/*
 
 ///////////////////////////////////////
 // Looping Objects: Object Keys, Values, and Entries
